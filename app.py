9deefacc21b3290881f44fb35703e4cf4ecc208d
scottkim0309 <kimjh03094823@gmail.com>
2026-07-20 10:35:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("AI챗봇 만들기")
-st.header("오늘의 질문")
-st.subheader("입력안내")
-st.write("질문을 자유롭게 입력")
+st.markdown("# AI챗봇 만들기")
+st.markdown("---")
+st.markdown("## 입력안내")
+st.markdown(" **제작언어**: "Python")
+st.markdown("### "최근대화")
+st.markdown("~~질문을 자유롭게 입력~~")
