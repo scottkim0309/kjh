febada887cebfc9859c5c81258f3b379df8f49c2
scottkim0309 <kimjh03094823@gmail.com>
2026-07-20 10:36:27 +0900

    Fix markdown syntax for language representation

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.markdown("# AI챗봇 만들기")
 st.markdown("---")
 st.markdown("## 입력안내")
-st.markdown(" **제작언어**: "Python")
+st.markdown(" **제작언어**: 'Python'")
 st.markdown("### "최근대화")
 st.markdown("~~질문을 자유롭게 입력~~")
