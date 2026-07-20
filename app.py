d56f924aec61fb748a14794a86ae873c10c7a275
scottkim0309 <kimjh03094823@gmail.com>
2026-07-20 14:38:29 +0900

    Initialize start_time in session state

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import time
 
 def reset_game():
-    st.session_state.start_time
-    st.session_state.end_time
+    st.session_state.start_time="0"
 
 if 'start_time' not in st.session_state:
     reset_game()
