ac635cc23b06fa6d28704058ef1600bd64fad6f1
scottkim0309 <kimjh03094823@gmail.com>
2026-07-20 14:03:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+def reset_all():
+    st.session_state.user_name = ""
+    st.session_state.weather = "맑음"
+    st.session_state.top_type = "후드티"
+    st.session_state.top_color = "밝음"
+    st.session_state.bottom_type = "청바지"
+    st.session_state.bottom_color = "슬림"
+    st.session_state.shoes = "스니커즈"
+    st.session_state.acc = []
+
+st.button("전체 초기화", on_click=reset_all)
 with st.sidebar:
     st.header("프로필")
     user_name = st.text_input("닉네임")
