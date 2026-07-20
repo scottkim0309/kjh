bfb82248b7e750aa336ef065e925e0c796140708
scottkim0309 <kimjh03094823@gmail.com>
2026-07-20 11:21:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.header("앱 UI만들기")
-user_id = st.text_input("이", placeholder="이름")
+user_id = st.text_input("이름", placeholder="이름")
 ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
 tone = st.selectbox("반", ["1", "2", "3","4","5","6"])
 creativity = st.select_slider("난이도",options=["쉬움", "보통", "어려움"],value="보통")
@@ -9,9 +9,6 @@ ai_speed = st.slider("점수",0,100,50)
 question = st.text_area("소감", placeholder="소감입력.")
 st.markdown("---")
 if st.button("확인"):
-        st.markdown(f"""
-        * **소감:** {question}
-        * **선택 :** `{ai_model}` | **말투:** `{tone}`
-        * **활성화 기능:** {', '.join(features) if features else '없음'}
-        * **창의성:** `{creativity}%` | **처리 속도:** `{ai_speed}`
-        """)
+        st.success(f"{user_id} / {ai_model}학년 / {tone}반 / {creaticity}")
+        st.markdown(f"점수: `{ai_speed}`")
+        st.info(f"소감: {question}")
