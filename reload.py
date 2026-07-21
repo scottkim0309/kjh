e508407b103ee5c75e334405d3350f7942a9d7d0
scottkim0309 <kimjh03094823@gmail.com>
2026-07-21 12:47:23 +0900

    Update reload.py

--- reload.py
+++ reload.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.session_state.user_name=()
+st.session_state.user_name=(user_name)
 with st.sidebar:
   st.header("닉네임 입력")
   user_name = st.text_input("닉네임", placeholder="게임닉을 입력해주세요")
