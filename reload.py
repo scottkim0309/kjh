2cfbd44cf847af1ce90c45c80979447f48a4e4e7
scottkim0309 <kimjh03094823@gmail.com>
2026-07-21 12:48:50 +0900

    Update reload.py

--- reload.py
+++ reload.py
@@ -8,7 +8,7 @@ with st.sidebar:
         st.warning("⚠️ 내용을 작성하지 않았습니다! 입력 후 다시 시도해 주세요.")
     else:st.success("있는계정입니다")
 st.title("😎이색 바텀조합 선택")
-st.write(f"🎮 **{st.session_state.user_name}**님, 원하는 바텀 조합을 선택해보세요!")
+st.write(f"🎮 **{user_name}**님, 원하는 바텀 조합을 선택해보세요!")
 st.markdown("---")
 st.subheader("바텀조합")
 Champion = st.radio("바텀조합", ["세나,사이온","세라핀,애쉬","야스오,요네","직스,레오나","베이가,알리스타","브랜드,럭스","럭스,소나","모르가나,파이크","스웨인,노틸러스","티모,신 짜오"], horizontal=True, label_visibility="collapsed")
