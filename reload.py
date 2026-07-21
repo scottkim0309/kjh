8ca7b4f94ba3edd8d7b5ebd9f188b5e22f50dc8a
scottkim0309 <kimjh03094823@gmail.com>
2026-07-21 12:17:37 +0900

    Update reload.py

--- reload.py
+++ reload.py
@@ -8,4 +8,4 @@ with st.sidebar:
         st.warning("⚠️ 내용을 작성하지 않았습니다! 입력 후 다시 시도해 주세요.")
     else:st.success("있는계정입니다")
 st.title("😎wildrift 탑챔프 추천")
-champion = st.text_input("챔프")
+champion = st.radio("챔프", [가렌,다리우스,아트록스,레넥톤,세트,볼리베어,피오라,요네,카밀,잭스,이렐리아,말파이트,오른,쉔,사이온], horizontal = true)
