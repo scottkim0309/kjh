fead978fd6a5c3f095af458004e8e256e3912f01
scottkim0309 <kimjh03094823@gmail.com>
2026-07-21 11:55:52 +0900

    Update reload.py

--- reload.py
+++ reload.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 with st.sidebar:
   st.header("닉네임 입력")
-  user_name = st.text_input("닉네임", key="user_name")
+  user_name = st.text_input("닉네임", key="user_name") st.placeholder("닉네임을 입력해주세요")
 st.title("😎wildrift 탑챔프 추천")
