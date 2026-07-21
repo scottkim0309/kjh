96408bdcc139ec04a3d555ca22a6f956a930a872
scottkim0309 <kimjh03094823@gmail.com>
2026-07-21 12:34:29 +0900

    Update reload.py

--- reload.py
+++ reload.py
@@ -7,6 +7,6 @@ with st.sidebar:
     if not user_name.strip():
         st.warning("⚠️ 내용을 작성하지 않았습니다! 입력 후 다시 시도해 주세요.")
     else:st.success("있는계정입니다")
-st.title("😎wildrift 탑챔프 추천")
-st.subheader("챔프선택")
-Champion = st.radio("챔프", ["가렌","다리우스","아트록스","레넥톤","볼리베어","피오라","요네","카밀","잭스","이렐리아","말파이트","오른","쉔","사이온","리븐","나서스","제이스"], horizontal=True, label_visibility="collapsed")
+st.title("😎wildrift 이색 바텀조합 선택")
+st.subheader("바텀조합")
+Champion = st.radio("바텀조합", ["가렌","다리우스","아트록스","레넥톤","볼리베어","피오라","요네","카밀","잭스","이렐리아","말파이트","오른","쉔","사이온","리븐","나서스","제이스"], horizontal=True, label_visibility="collapsed")
