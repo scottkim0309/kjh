2bbe6ded22f21235c2dc34e350f1969757c00b7e
scottkim0309 <kimjh03094823@gmail.com>
2026-07-21 12:05:26 +0900

    Update reload.py

--- reload.py
+++ reload.py
@@ -5,6 +5,6 @@ with st.sidebar:
   user_name = st.text_input("닉네임", placeholder="게임닉을 입력해주세요")
   if not user_input.strip(user_name):
         st.warning("⚠️ 내용을 작성하지 않았습니다! 입력 후 다시 시도해 주세요.")
-    else:
-        st.success("있는계정입니다")
+      else:
+      st.success("있는계정입니다")
 st.title("😎wildrift 탑챔프 추천")
