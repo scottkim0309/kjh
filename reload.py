53ee5f08a12f91baaa6da6cf92e21266e5fb695a
scottkim0309 <kimjh03094823@gmail.com>
2026-07-21 12:08:57 +0900

    Fix button check and user input validation

--- reload.py
+++ reload.py
@@ -3,7 +3,8 @@ import streamlit as st
 with st.sidebar:
   st.header("닉네임 입력")
   user_name = st.text_input("닉네임", placeholder="게임닉을 입력해주세요")
-  if not user_input.strip(user_name):
+  if st.buttton("확인")
+    if not user_name.strip():
         st.warning("⚠️ 내용을 작성하지 않았습니다! 입력 후 다시 시도해 주세요.")
-      else:st.success("있는계정입니다")
+    else:st.success("있는계정입니다")
 st.title("😎wildrift 탑챔프 추천")
